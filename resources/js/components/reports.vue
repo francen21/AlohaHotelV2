3666531cd35c87f7493a2e738a7553906744e037
ZandreeTresvalles <zandree.tresvalles@gmail.com>
2020-01-12 18:38:17 +0800

    beta

--- resources/js/components/reports.vue
+++ resources/js/components/reports.vue
@@ -130,10 +130,10 @@
 </template>
 
 <script>
-    import LineChart from './LineChart.js'
+    //import LineChart from './LineChart.js'
     export default {
         components: {
-            LineChart
+            //LineChart
         },
         data(){
             return{
@@ -141,25 +141,7 @@
             }
         },
         methods:{
-            fillData () {
-                this.datacollection = {
-                labels: [this.getRandomInt(), this.getRandomInt()],
-                datasets: [
-                    {
-                        label: 'Data One',
-                        backgroundColor: '#f87979',
-                        data: [this.getRandomInt(), this.getRandomInt()]
-                    }, {
-                        label: 'Data One',
-                        backgroundColor: '#f87979',
-                        data: [this.getRandomInt(), this.getRandomInt()]
-                    }
-                ]
-                }
-            },
-            getRandomInt () {
-                return Math.floor(Math.random() * (50 - 5 + 1)) + 5
-            }
+
         },
         mounted() {
             console.log('Component mounted.');
